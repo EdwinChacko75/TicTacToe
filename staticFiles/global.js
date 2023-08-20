2b559b08a76740d9e9fab23abca7cbcab2e24402
EdwinChacko75 <78929985+EdwinChacko75@users.noreply.github.com>
2023-08-20 17:48:15 -0400

    Static File Management

--- staticFiles/global.js
+++
@@ -1,6 +0,0 @@
-function clickEvent(element) {
-    clickedElement = element.target.id;
-    console.log(clickedElement);
-}
-console.log("main.js loaded")
-document.addEventListener('DOMContentLoaded', clickEvent);
--- ticTacToe/settings.py
+++ ticTacToe/settings.py
@@ -118,14 +118,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'staticFiles'
+STATIC_ROOT = BASE_DIR / 'static'
 
+
+# STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'tttgame/static']
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'mystaticfiles'
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
